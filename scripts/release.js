f0f67a114684f92296ef51bd5f6fed85f99ba998
Johan Sanneblad <johan@sanneblad.se>
2025-12-31 11:48:19 +0100

    Video script

--- scripts/release.js
+++ scripts/release.js
@@ -1,10 +1,24 @@
 #!/usr/bin/env node
 
 /*
- * Obsidian Plugin Release Script
- * ==============================
- * This script is checked into git
+ * Notebook Navigator - Plugin for Obsidian
+ * Copyright (c) 2025 Johan Sanneblad
  *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Release Script
+ * ==============
  * This script automates the release process for Obsidian plugins by:
  * - Incrementing version numbers in manifest.json, package.json, and versions.json
  * - Committing the changes
